37be217e0e69f2dcdf8e5083fa8a8b68d4bf3392
Michael I Chen <michael.chen.0@gmail.com>
2022-01-27 16:52:13 -0800

    Update load_frill_module.py

--- msvc/src/features/load_frill_module.py
+++ msvc/src/features/load_frill_module.py
@@ -12,7 +12,7 @@ def load_frill_module():
         import tensorflow as tf
         import tensorflow_hub as hub
 
-        tf.enable_v2_behavior()
+        # tf.enable_v2_behavior()
         frill_path = "msvc/src/features/FRILL"
         # frill_path = "https://tfhub.dev/google/nonsemantic-speech-benchmark/frill/1"
         return hub.load(frill_path)
