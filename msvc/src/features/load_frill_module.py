1096cc397b33e219d0d907f9334d2158c8a197f5
Michael I Chen <michael.chen.0@gmail.com>
2022-01-27 21:28:57 -0800

    Update requirements.txt

--- msvc/src/features/load_frill_module.py
+++ msvc/src/features/load_frill_module.py
@@ -8,11 +8,10 @@ import streamlit as st
 def load_frill_module():
     """returns the FRILL module as a tensorflow _UserObject"""
     with st.spinner("thank you for your patience..."):
-        # import tensorflow.compat.v2 as tf
-        import tensorflow as tf
+        import tensorflow.compat.v2 as tf
         import tensorflow_hub as hub
 
-        # tf.enable_v2_behavior()
+        tf.enable_v2_behavior()
         frill_path = "msvc/src/features/FRILL"
         # frill_path = "https://tfhub.dev/google/nonsemantic-speech-benchmark/frill/1"
         return hub.load(frill_path)
